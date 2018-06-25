bb2793b76a4dd09bb7fd1b66bfde6d6080d97948
cassidytrinh <cassidytrinh@gmail.com>
2018-06-25 14:23:49 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
 class BankAccount
-  # code here
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def balance
+    @balance
+  end
+
+  def status
+    @status
+  end
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    return "Your Balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      return true
+    end
+    return false
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
